d99ac804c55c37fc526307aeb8a6843d6feb950a
Mark <mark.hall@work.room3b.eu>
2014-10-02 20:31:27 +0100

    Added Vlad's gazetteer with data.

--- src/pycaptioner/gazetteer.py
+++ src/pycaptioner/gazetteer.py
@@ -5,6 +5,9 @@ u"""
 """
 import numpy
 
+from re import match
+from shapely import geometry, wkt
+
 POI = 'POI'
 JUNCTION = 'JUNCTION'
 TOWN = 'TOWN'
@@ -22,3 +25,53 @@ GAZETTEER = [{'geo_lonlat': numpy.array((-2.6001763343811035, 53.38953524087438)
               'dc_title': 'Sankey Street and Arpley Street',
               'dc_type': JUNCTION,
               'tripod_score': 1}]
+
+
+class VladGazetteer(object):
+
+    def __init__(self, source):
+        self._gazetteer = {}
+        for line in source:
+            line = line.split('||')
+            reference = '%f::%f' % (float(line[1]), float(line[0]))
+            if reference not in self._gazetteer:
+                self._gazetteer[reference] = {'poi': [], 'way': [], 'contain': []}
+            if line[2] == 'Toponym':
+                line[8] = line[8].strip()[15:-1]
+                topo = {'geo_lonlat': geometry.Point(float(line[8].split(',')[0]), float(line[8].split(',')[1])),
+                        'dc_title': line[3],
+                        'dc_type': line[4] if line[4] != 'null' else None,
+                        'dc_source': line[5],
+                        'tripod_score': int(line[7])}
+                self._gazetteer[reference]['poi'].append(topo)
+            elif line[2] == 'Ways':
+                topo = {'geo_lonlat': wkt.loads(line[7]),
+                        'dc_title': line[3],
+                        'dc_type': line[4] if line[4] != 'null' else None,
+                        'dc_source': line[5],
+                        'tripod_score': int(line[6])}
+                self._gazetteer[reference]['way'].append(topo)
+            elif line[2] == 'Hierarchy':
+                for part in line[3].strip()[1:-1].split(','):
+                    m = match(r'([a-zA-Z ]+)\(([a-zA-Z -]+)\)', part.strip())
+                    if m:
+                        topo = {'geo_lonlat': None,
+                                'dc_title': m.group(1),
+                                'dc_type': m.group(2),
+                                'dc_source': None,
+                                'tripod_score': 0}
+                        self._gazetteer[reference]['contain'].append(topo)
+
+    def __len__(self):
+        total = 0
+        for categories in self._gazetteer.values():
+            for points in categories.values():
+                total = total + len(points)
+        return total
+
+    def __call__(self, point, category):
+        reference = '%f::%f' % (point.x, point.y)
+        if reference in self._gazetteer and category in self._gazetteer[reference]:
+            return self._gazetteer[reference][category]
+        else:
+            return None
