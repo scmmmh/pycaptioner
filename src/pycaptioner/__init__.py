b4d8fa5f93b49c3214f4ef0547c8005f654eabc1
Mark <Mark.Hall@edgehill.ac.uk>
2015-12-01 20:44:59 +0000

    Added support for generating the test data.

--- src/pycaptioner/__init__.py
+++ src/pycaptioner/__init__.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import numpy
 
@@ -5,7 +6,8 @@ from pycaptioner import models, generator, language
 
 logging.root.setLevel(logging.DEBUG)
 
-def main():
+
+def local_test():
     points = [
               (-2.63629, 53.39797), # Dakota Park
               (-1.88313, 53.38129), # Peak District
@@ -23,3 +25,22 @@ def main():
     for c in captions:
         print(c)
 
+
+def eval_test():
+    captions = []
+    with open('src/pycaptioner/test/data/points.csv') as in_f:
+        reader = csv.DictReader(in_f)
+        for line in reader:
+            point = (float(line['lon']), float(line['lat']))
+            print(point)
+            caption = generator.generate_caption(point)
+            caption.insert(0, {'dc_type': 'string', 'value': line['subject']})
+            caption = language.generate_caption(caption)
+            print(caption)
+            captions.append(caption)
+    for c in captions:
+        print(c)
+
+
+def main():
+    eval_test()
