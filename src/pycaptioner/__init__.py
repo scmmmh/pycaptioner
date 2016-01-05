2193f4cec40996bca40035257b3502e7ca4aee9f
Mark <mark.hall@work.room3b.eu>
2016-01-05 12:14:43 +0000

    Added runnable main to localise one point.

--- src/pycaptioner/__init__.py
+++ src/pycaptioner/__init__.py
@@ -1,9 +1,9 @@
-import logging
-import numpy
+import json
 
-from pyproj import Proj
+from argparse import ArgumentParser
+from shapely import wkt
 
-from pycaptioner import models, language, generator
+from pycaptioner import configurations, generator, language
 from pycaptioner.gazetteer import *
 
 logging.root.setLevel(logging.DEBUG)
@@ -15,36 +15,32 @@ URBAN_TWO_POINT_MODELS = ['between.urban']
 
 
 def main():
-    proj = Proj(init='epsg:32630')
-    for feature in GAZETTEER:
-        feature['geo_lonlat'] = numpy.array(proj(*feature['geo_lonlat']))
-    #point = numpy.array(proj(*(-2.5901763343811035, 53.38953524087438)))
-    #point = numpy.array(proj(*(-2.6008307933807373, 53.388645857875055, )))
-    #point = numpy.array(proj(*(-2.5901763343811035, 53.38953524087438)))
-    point = numpy.array(proj(*(-2.6004, 53.3887))) # Between point
-    configurations = []
-    """
-    for model_name in RURAL_ONE_POINT_MODELS:
-        model = models.load(model_name)
-        for feature in GAZETTEER:
-            if model_name in ['at.urban', 'near.urban', 'next_to.urban', 'east.rural', 'north.rural', 'west.rural', 'south.rural', 'near.rural'] and feature['dc_type'] == POI:
-                value = model(*(point - feature['geo_lonlat']))
-            elif model_name == 'at_corner.urban' and feature['dc_type'] == JUNCTION:
-                value = model(*(point - feature['geo_lonlat']))
-            else:
-                value = None
-            if value and value >= 0.6:
-                print model_name, value
-                configurations.append(((model_name, value), feature))
-    """
-    for model_name in RURAL_TWO_POINT_MODELS:
-        model = models.load(model_name)
-        for feature1 in GAZETTEER:
-            for feature2 in GAZETTEER:
-                if feature1['dc_type'] != JUNCTION and feature2['dc_type'] != JUNCTION and feature1['dc_title'] != feature2['dc_title']:
-                    baseline = feature2['geo_lonlat'] - feature1['geo_lonlat']
-                    pos = point - feature1['geo_lonlat']
-                    print(baseline, pos)
-                    configurations.append((('between.rural', 1), feature1, feature2))
-    print(language.generate_caption(generator.urban_caption(configurations)))
-
+    parser = ArgumentParser()
+    parser.add_argument('lon', help='Point longitude')
+    parser.add_argument('lat', help='Point latitude')
+    parser.add_argument('gaz', help='File to load the gazetteer from')
+    parser.add_argument('category', choices=['urban', 'rural'], help='Generate an urban or rural caption')
+    parser.add_argument('--subject', help='Image subject')
+    args = parser.parse_args()
+    point = geometry.Point(float(args.lon), float(args.lat))
+    with open(args.gaz) as in_f:
+        gaz = VladGazetteer(in_f)
+    configs = configurations.generate_configurations(point, gaz, args.category)
+    if args.subject:
+            configurations['subject'] = {'dc_title': args.subject}
+    if args.category == 'rural':
+        caption = generator.rural_caption(configs)
+    else:
+        caption = generator.rural_caption(configs)
+    rendered_caption = language.generate_caption(caption)
+    for element in caption:
+        if 'feature' in element:
+            if 'geo_type' in element['feature']:
+                element['feature']['geo_type'] = {'main': element['feature']['geo_type'].main_type,
+                                                  'sub': element['feature']['geo_type'].sub_type,
+                                                  'urban_score': element['feature']['geo_type'].urban_score,
+                                                  'rural_score': element['feature']['geo_type'].rural_score}
+            if 'geo_lonlat' in element['feature'] and element['feature']['geo_lonlat']:
+                element['feature']['geo_lonlat'] = wkt.dumps(element['feature']['geo_lonlat'])
+    print(json.dumps({'caption': rendered_caption,
+                      'source': caption}))
