0ae8b334282182fdd2edb4b2cd53e88e336be22a
Mark <Mark.Hall@edgehill.ac.uk>
2016-01-05 11:24:47 +0000

    Added functionality for generating a single caption on the command-line.

--- src/pycaptioner/__init__.py
+++ src/pycaptioner/__init__.py
@@ -1,13 +1,14 @@
 import csv
+import json
 import logging
 import numpy
 
-from pycaptioner import models, generator, language
-
-logging.root.setLevel(logging.DEBUG)
-
-
-def local_test():
+from argparse import ArgumentParser
+from shapely import wkt
+ 
+def local_test(args):
+    """Run the set of local test points"""
+    from pycaptioner import generator, language
     points = [
               (-2.63629, 53.39797),  # Dakota Park
               (-1.88313, 53.38129),  # Peak District
@@ -21,20 +22,22 @@ def local_test():
     captions = []
     for point in points:
         print(point)
-        caption = generator.generate_caption(point)
+        caption = generator.generate_caption(args.sqlalchemy_url, point)
         captions.append(language.generate_caption(caption))
     for c in captions:
         print(c)
 
 
-def eval_test():
+def eval_test(args):
+    """Run the set of internal evaluation points"""
+    from pycaptioner import generator, language
     captions = []
     with open('src/pycaptioner/test/data/points.csv') as in_f:
         reader = csv.DictReader(in_f)
         for line in reader:
             point = (float(line['lon']), float(line['lat']))
             print(point)
-            caption = generator.generate_caption(point, filter_names=[line['subject']])
+            caption = generator.generate_caption(args.sqlalchemy_url, point, filter_names=[line['subject']])
             caption.insert(0, {'dc_type': 'string', 'value': line['subject']})
             caption = language.generate_caption(caption)
             print(caption)
@@ -43,5 +46,40 @@ def eval_test():
         print(c)
 
 
+def point_caption(args):
+    """Generate a caption for a single point"""
+    from pycaptioner import generator, language
+    point = (float(args.lon), float(args.lat))
+    caption = generator.generate_caption(args.sqlalchemy_url, point, filter_names=[args.subject] if args.subject else None)
+    if args.subject:
+        caption.insert(0, {'dc_type': 'string', 'value': args.subject})
+    rendered_caption = language.generate_caption(caption)
+    for element in caption:
+        if 'toponym' in element and 'osm_geometry' in element['toponym']:
+            element['toponym']['osm_geometry'] = wkt.dumps(element['toponym']['osm_geometry'])
+    print(json.dumps({'caption': rendered_caption,
+                      'source': caption}))
+
+
 def main():
-    eval_test()
+    """Main application function."""
+    parser = ArgumentParser()
+    parser.add_argument('sqlalchemy_url', help='The SQLAlchemy connection URL')
+    parser.add_argument('-v', action='store_true', help='Verbose logging (INFO level)')
+    parser.add_argument('-vv', action='store_true', help='Very verbose logging (DEBUG level)')
+    sub_parsers = parser.add_subparsers()
+    sub_parser = sub_parsers.add_parser('local-test')
+    sub_parser.set_defaults(func=local_test)
+    sub_parser = sub_parsers.add_parser('eval-test')
+    sub_parser.set_defaults(func=eval_test)
+    sub_parser = sub_parsers.add_parser('point')
+    sub_parser.set_defaults(func=point_caption)
+    sub_parser.add_argument('lon', help='Point longitude')
+    sub_parser.add_argument('lat', help='Point latitude')
+    sub_parser.add_argument('--subject', help='Image subject')
+    args = parser.parse_args()
+    if args.v:
+        logging.root.setLevel(logging.INFO)
+    if args.vv:
+        logging.root.setLevel(logging.DEBUG)
+    args.func(args)
